dd6a9dd0d390fd53d53cbce63eca03fd03859e9b
Basilio Briceño <basilio@bluespark.com>
2017-02-23 17:59:32 -0600

    Adding PHP7 extension

--- httpd/habitat/plan.sh
+++ httpd/habitat/plan.sh
@@ -7,7 +7,7 @@ pkg_maintainer="Basilio Briceno <bbh@briceno.mx>"
 pkg_license=('Apache-2.0')
 pkg_source="https://archive.apache.org/dist/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz"
 pkg_shasum=be6c5eb805216ec205453bb02b1990c82609cb1b145bcb69dc6e99fff45493a9
-pkg_deps=(core/glibc core/expat core/libiconv core/apr core/apr-util core/pcre core/zlib core/openssl core/gcc-libs)
+pkg_deps=(core/glibc core/expat core/libiconv core/apr core/apr-util core/pcre core/zlib core/openssl core/gcc-libs core/perl)
 pkg_build_deps=(core/patch core/make core/gcc)
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
@@ -33,12 +33,16 @@ do_build ()
     --enable-modules="none" \
     --enable-mods-static="none" \
     --enable-mods-shared="reallyall" \
-    --enable-mpms-shared="prefork event worker"
+    --enable-mpms-shared="prefork event worker" \
+    --enable-so
   make -j4
 }
 
 do_install ()
 {
   make install
+  if [ -e ${PLAN_CONTEXT}/../files/libphp7.so ]; then
+    cp -vr ${PLAN_CONTEXT}/../files/libphp7.so $pkg_prefix/modules/
+  fi
   cp -rv ${PLAN_CONTEXT}/../source/* ${pkg_prefix}/htdocs/
 }
