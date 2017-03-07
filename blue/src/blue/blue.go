a8a7687d984d62fd9e5e1cb1351eaae7a906d00d
Basilio Briceño <basilio@bluespark.com>
2017-03-06 21:40:06 -0600

    Adding support for SQL file

--- blue/src/blue/blue.go
+++ blue/src/blue/blue.go
@@ -14,16 +14,14 @@ func msg_error(error_message string) {
 func load_config() {
   // Declare variables
   var project interface{}
-  var repository string = ""
   var toml_location string = "blue.toml"
-  // Validate TOML file location
+  // Validate and Load TOML file
   if _, err := os.Stat(toml_location); os.IsNotExist(err) {
     msg_error("TOML file not found")
   }
-  // Load TOML content
   tree, err := toml.LoadFile(toml_location)
   if err != nil {
-    fmt.Println("ERROR ", err.Error())
+    fmt.Println("[TOML ERROR]", err.Error())
   } else {
     // Validate blue group
     if !tree.Has("blue") {
@@ -37,6 +35,14 @@ func load_config() {
     repository := config.Get("repo").(string)
     if _, err := os.Stat(repository); os.IsNotExist(err) {
       msg_error("[CONFIG] Repository path not found")
+    }
+    // Validate SQL dump
+    if !config.Has("dump") {
+      msg_error("[CONFIG] SQL dump path it's required")
+    }
+    sqldump := config.Get("dump").(string)
+    if _, err := os.Stat(sqldump); os.IsNotExist(err) {
+      msg_error("[CONFIG] SQL dump path not found")
     }
 
     // Print out test result
