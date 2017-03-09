961aad7ce6a457569fd359dce3ffb8dd0be8267c
Basilio Briceño <basilio@bluespark.com>
2017-03-08 19:02:13 -0600

    Clean up code

--- blue/src/blue/blue.go
+++ blue/src/blue/blue.go
@@ -6,9 +6,13 @@ import (
   "github.com/pelletier/go-toml"
 )
 
-func msg_error(error_message string) {
+func msg_error(message string) {
   fmt.Println("[ERROR]", error_message)
   os.Exit(1)
+}
+
+func msg_info(message string) {
+  fmt.Println("[INFO]", message)
 }
 
 func load_config() {
@@ -21,42 +25,35 @@ func load_config() {
   }
   tree, err := toml.LoadFile(toml_location)
   if err != nil {
-    fmt.Println("[TOML ERROR]", err.Error())
+    msg_error(err.Error())
   } else {
     // Validate group project
     if !tree.Has("project") {
-      msg_error("[CONFIG] Project group it's required")
+      msg_error("[CONF] Group project it's required")
     }
     g_project := tree.Get("project").(*toml.TomlTree)
     // Validate repository location
     if !g_project.Has("location") {
-      msg_error("[CONFIG] Project's code location it's required")
+      msg_error("[CONF] Project's code location it's required")
     }
     location := g_project.Get("location").(string)
     if _, err := os.Stat(location); os.IsNotExist(err) {
-      msg_error("[CONFIG] Project's code location not found")
+      msg_error("[CONF] Project dir "+ location +" not found")
     }
     // Validate schemas
     if tree.Has("schema") {
       schemas := tree.Get("schema").(*toml.TomlTree)
       for _, schema_key := range schemas.Keys() {
         schema := schemas.Get(schema_key).(*toml.TomlTree)
-        fmt.Println(schema.Get("hostname"))
         // Validate SQL dump
         if schema.Has("sql_dump") {
           sqldump := schema.Get("sql_dump").(string)
           if _, err := os.Stat(sqldump); os.IsNotExist(err) {
-            msg_error("[CONFIG] SQL dump not found")
+            msg_error("[CONF] File ("+ sqldump +") not found")
           }
         }
       }
     }
-
-    // Print out test result
-    if g_project.Has("name") {
-      project = g_project.Get("name").(string)
-    }
-    fmt.Println("The repo path for project:", project, "is", location)
   }
 }
 
