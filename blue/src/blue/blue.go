9b5f1c9ecc743d524c4d9675c535272e843204d2
Basilio Briceño <basilio@bluespark.com>
2017-03-09 20:19:11 -0600

    Validate stack and packages

--- blue/src/blue/blue.go
+++ blue/src/blue/blue.go
@@ -4,10 +4,12 @@ import (
   "fmt"
   "os"
   "github.com/pelletier/go-toml"
+  "strconv"
+  "reflect"
 )
 
 func msg_error(message string) {
-  fmt.Println("[ERROR]", error_message)
+  fmt.Println("[ERROR]", message)
   os.Exit(1)
 }
 
@@ -17,7 +19,7 @@ func msg_info(message string) {
 
 func load_config() {
   // Declare variables
-  var project interface{}
+  // var project interface{}
   var toml_location string = "blue.toml"
   // Validate and Load TOML file
   if _, err := os.Stat(toml_location); os.IsNotExist(err) {
@@ -53,6 +55,26 @@ func load_config() {
           }
         }
       }
+    }
+    // Validate stack
+    if !tree.Has("stack") {
+      msg_error("[CONF] Group stack it's required")
+    }
+    // Validate packages
+    // TODO Validate if array it's empty
+    if !tree.Has("stack.packages") {
+      msg_error("[CONF] Stack's packages required")
+    }
+    rt := reflect.TypeOf(tree.Get("stack.packages"))
+    if rt.Kind() != reflect.Slice {
+      msg_error("[CONF] Stack's packages attribute should be an array")
+    }
+    // Load primary results to add extra slice (workaround)
+    pres,_ := tree.Query("$.stack.packages[0:-1]")
+    items := len(pres.Values())+1 // Workaround to get all the nodes
+    results,_ := tree.Query("$.stack.packages[0:"+strconv.Itoa(items)+"]")
+    for _,item := range results.Values() {
+      fmt.Println(item)
     }
   }
 }
