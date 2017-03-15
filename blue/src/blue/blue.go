d35b6b3d37a206e7539f64990e5e56bae2e1613f
Basilio Briceño <basilio@bluespark.com>
2017-03-15 15:31:35 -0600

    Separated blue functions from common functions and fixed home(~) dir issue

--- blue/src/blue/blue.go
+++ blue/src/blue/blue.go
@@ -1,166 +1,17 @@
 package main
 
 import (
-  "io"
   "os"
   "fmt"
-  "regexp"
   "runtime"
   "strconv"
-  "strings"
   "reflect"
-  "net/http"
-  "archive/tar"
-  "archive/zip"
   "path/filepath"
-  "compress/gzip"
   "github.com/pelletier/go-toml"
 )
 
-func msg_error(message string) {
-  fmt.Println("[ERROR]", message)
-  os.Exit(1)
-}
-
-func msg_info(message string) {
-  fmt.Println("[INFO]", message)
-}
-
-func download_file(filepath string, url string) (err error) {
-  // Create the file
-  out, err := os.Create(filepath)
-  defer out.Close()
-  if err != nil  {
-    return err
-  }
-  // Get the data
-  resp, err := http.Get(url)
-  defer resp.Body.Close()
-  if err != nil {
-    return err
-  }
-  // Writer the body to file
-  _, err = io.Copy(out, resp.Body)
-  if err != nil  {
-    return err
-  }
-  return nil
-}
-
-func get_home() string {
-  var home string
-  for _, env_var := range os.Environ() {
-    match, err := regexp.MatchString("HOME=(.*)", env_var)
-    if (err != nil) {
-      msg_error("Cannot determine environment variable HOME")
-    } else if match {
-      home = strings.Split(env_var,"=")[1]
-      break
-    }
-  }
-  return home
-}
-
-func overwrite(file_path string) (*os.File, error) {
-  f, err := os.OpenFile(file_path, os.O_RDWR|os.O_TRUNC, 0777)
-  if err != nil {
-    f, err = os.Create(file_path)
-    if err != nil {
-      return f, err
-    }
-  }
-  return f, nil
-}
-
-func read(file_path string) (*os.File, error) {
-  f, err := os.OpenFile(file_path, os.O_RDONLY, 0444)
-  if err != nil {
-    return f, err
-  }
-  return f, nil
-}
-
-func untar(tar_path string, dest_path string) {
-  fr, err := read(tar_path)
-  defer fr.Close()
-  if err != nil {
-    panic(err)
-  }
-  gr, err := gzip.NewReader(fr)
-  defer gr.Close()
-  if err != nil {
-    panic(err)
-  }
-  tr := tar.NewReader(gr)
-  for {
-    hdr, err := tr.Next()
-    if err == io.EOF {
-      // end of tar archive
-      break
-    }
-    if err != nil {
-      panic(err)
-    }
-    path := dest_path+"/"+hdr.Name
-    switch hdr.Typeflag {
-    case tar.TypeDir:
-      if err := os.MkdirAll(path, os.FileMode(hdr.Mode)); err != nil {
-        panic(err)
-      }
-    case tar.TypeReg:
-      ow, err := overwrite(path)
-      defer ow.Close()
-      if err != nil {
-        panic(err)
-      }
-      if _, err := io.Copy(ow, tr); err != nil {
-        panic(err)
-      }
-    default:
-      fmt.Printf("Can't: %c, %s\n", hdr.Typeflag, path)
-    }
-  }
-}
-
-func unzip(src, dest string) error {
-  r, err := zip.OpenReader(src)
-  defer r.Close()
-  if err != nil {
-    return err
-  }
-  for _, f := range r.File {
-    rc, err := f.Open()
-    defer rc.Close()
-    if err != nil {
-      return err
-    }
-    fpath := filepath.Join(dest, f.Name)
-    if f.FileInfo().IsDir() {
-      os.MkdirAll(fpath, f.Mode())
-    } else {
-      var fdir string
-      if lastIndex := strings.LastIndex(fpath, string(os.PathSeparator)); lastIndex > -1 {
-        fdir = fpath[:lastIndex]
-      }
-      err = os.MkdirAll(fdir, f.Mode())
-      if err != nil {
-        return err
-      }
-      f, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-      defer f.Close()
-      if err != nil {
-        return err
-      }
-      _, err = io.Copy(f, rc)
-      if err != nil {
-        return err
-      }
-    }
-  }
-  return nil
-}
-
 func hab_install(tree *toml.TomlTree) string {
+  // Validate bsp_path
   if !tree.Has("habitat.bsp_path") {
     msg_error("[CONF] Group habitat it's required")
   }
@@ -235,15 +86,12 @@ func hab_install(tree *toml.TomlTree) string {
 }
 
 func load_config() {
-  // Declare variables
-  // var project interface{}
   var toml_location string = "blue.toml"
   // Validate and Load TOML file
   if _, err := os.Stat(toml_location); os.IsNotExist(err) {
     msg_error("TOML file not found")
   }
-  tree, err := toml.LoadFile(toml_location)
-  if err != nil {
+  if tree, err := toml.LoadFile(toml_location); err != nil {
     msg_error(err.Error())
   } else {
     // Validate group project
@@ -252,12 +100,12 @@ func load_config() {
     }
     g_project := tree.Get("project").(*toml.TomlTree)
     // Validate repository location
-    if !g_project.Has("location") {
+    if !g_project.Has("path") {
       msg_error("[CONF] Project's code location it's required")
     }
-    location := g_project.Get("location").(string)
+    location := get_abs(g_project.Get("path").(string))
     if _, err := os.Stat(location); os.IsNotExist(err) {
-      msg_error("[CONF] Project dir "+ location +" not found")
+      msg_error("[CONF] Directory "+ location +" not found")
     }
     // Validate schemas
     if tree.Has("schema") {
@@ -268,7 +116,10 @@ func load_config() {
         if schema.Has("sql_dump") {
           sqldump := schema.Get("sql_dump").(string)
           if _, err := os.Stat(sqldump); os.IsNotExist(err) {
-            msg_error("[CONF] File ("+ sqldump +") not found")
+            sqldump = filepath.Join(location, sqldump)
+            if _, err := os.Stat(sqldump); os.IsNotExist(err) {
+              msg_error("[CONF] File ("+ sqldump +") not found")
+            }
           }
         }
       }
