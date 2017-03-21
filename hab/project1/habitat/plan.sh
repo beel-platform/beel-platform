f7576262e053579122cdb0cb3c8201b9574afb72
Basilio Briceño <basilio@bluespark.com>
2017-03-20 18:35:30 -0600

    Adding project1 as example

---
+++ hab/project1/habitat/plan.sh
@@ -0,0 +1,36 @@
+pkg_origin=bluespark
+pkg_name=myproject
+pkg_version=0.1.0
+pkg_description="Project number one"
+pkg_maintainer="Basilio Briceño <basilio@bluespark.com>"
+pkg_license=()
+pkg_source=nosuchfile.tar.gz
+pkg_deps=(bluespark/httpd bluespark/php7 bluespark/mysql)
+pkg_expose=(80 443 3306)
+# pkg_svc_user=root
+# pkg_svc_group=root
+
+do_download ()
+{
+  return 0
+}
+
+do_verify ()
+{
+  return 0
+}
+
+do_unpack ()
+{
+  return 0
+}
+
+do_build ()
+{
+  return 0
+}
+
+do_install ()
+{
+  return 0
+}
